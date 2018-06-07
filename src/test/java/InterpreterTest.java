ffb020408bfa3c39205df454ec29cc5a198e136f
AlternateData <louiskronberg@gmail.com>
2018-06-07 20:23:36 +0200

    new test prepping

--- src/test/java/InterpreterTest.java
+++ src/test/java/InterpreterTest.java
@@ -2,18 +2,9 @@ package test.java;
 
 import main.java.Interpreter;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ConditionEvaluationResult;
-import org.junit.jupiter.api.extension.ExecutionCondition;
-import org.junit.jupiter.api.extension.ExtensionContext;
 
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-import java.util.Map;
 import java.util.List;
-import javafx.util.Pair;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -33,27 +24,27 @@ public class InterpreterTest {
                                              "[[[]]]1234",};
 
 
-
-    public final static int MAX_TEST_RUNS =  10;
-
-
-
-    byte[] desiredResults_JumpLength = {4,6, 5, 1,3};
+    byte[] desiredResults_JumpLength = {5, 6, 5, 1, 3};
     int[]  starts = {3,3,0,0,1};
 
-    public void executeTestCases(){
-        
+    public void executeTestCases(List<String> testCases, List<String> desiredResults){
+
+
     }
 
+
+
     @DisplayName("jumpLength")
-    public void testJumpLength(RepetitionInfo repinfo){
+    @Test
+    public void testJumpLengthCase(){
         int start, len0, len1;
         char[] program;
         for(int i=0; i<programSet_JumpLength.length;i++){
             program = programSet_JumpLength[i].toCharArray();
             start = starts[i];
             len0 = testbox.jumpLength(program, start);
-            assertEquals(len0, desiredResults_JumpLength[i], "Failed to compute the length of a jump towards ]");
+            System.out.println(Integer.toString(len0) + " " + Integer.toString(desiredResults_JumpLength[i]));
+            assertEquals(len0, desiredResults_JumpLength[i], String.format("Failed to compute the length of a jump towards ] (%d)", i));
             len1 = testbox.jumpLength(program, start + len0);
             assertEquals(len0, -len1,"Failed to compute the length of a jump towards [");
         }
