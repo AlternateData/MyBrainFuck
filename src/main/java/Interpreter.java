cadecfd577930e439e12734008ed7c0ccbcf2164
AlternateData <louiskronberg@gmail.com>
2018-06-08 13:53:06 +0200

    added logging and added testInterpret

--- src/main/java/Interpreter.java
+++ src/main/java/Interpreter.java
@@ -1,13 +1,21 @@
 package main.java;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.Level;
 
 public final class Interpreter {
 
     // TODO: retire the programm array and instead load the program directly into memory/tape array
     // TODO: replace VALID_INSTRUCTIONS with an enum, this would make things more flexible
     // TODO: move out the validate methods into a Parser class and create an additional engine class
+    // TODO: change output and input in our giant switch statement
 
     final static char[] VALID_INSTRUCTIONS = {'>', '<', '+', '-', '.', ',', '[', ']'};
 
@@ -19,6 +27,23 @@ public final class Interpreter {
 
     private final  Scanner scanner = new Scanner(System.in);
 
+    static Logger logger;
+
+
+    // this inits the logger
+    static{
+        logger = Logger.getLogger(Interpreter.class.getName());
+        logger.setLevel(Level.FINE);
+        Handler handler;
+        SimpleFormatter formatter = new SimpleFormatter();
+        try {
+            handler = new FileHandler("../resources/log.txt");
+            handler.setFormatter(formatter);
+            logger.addHandler(handler);
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
 
     public Interpreter(){
 
@@ -69,14 +94,20 @@ public final class Interpreter {
         this.program = program.toCharArray();
     }
 
+    // compute the distance from the bracket at program[start] to its closing bracket
+    // if there is no bracket at program[start] an IllegalArgumentException is raised
     public int jumpLength(char[] program, int start){
         int skip = 0;
         int len = 0;
 
-        if(program.length < start)
+        if(program.length < start) {
+            logger.severe("Illegal index into progam");
             throw new IndexOutOfBoundsException("Illegal index into progam");
-        if(program[start] != '[' && program[start] != ']')
+        }
+        if(program[start] != '[' && program[start] != ']') {
+            logger.severe("Character at index start is neither [ nor ] but is " + program[start]);
             throw new IllegalArgumentException("Character at index start is not [ or ]");
+        }
 
         int i = start + 1;
         int di = 1;
@@ -96,7 +127,7 @@ public final class Interpreter {
             ithInstruction = program[i];
             if(ithInstruction == seen) {
                 skip++;
-            }else if(ithInstruction == search && skip != 0){
+            }else if(ithInstruction == search && skip > 0){
                 skip--;
             }else if(ithInstruction == search){
                 len = i - start;
@@ -121,9 +152,12 @@ public final class Interpreter {
 
     public byte interpret(char[] program) throws NullPointerException, IllegalArgumentException{
         //load_program(program);
-        if(program == null)
+        if(program == null) {
+            logger.severe("No program to interpret was given");
             throw new NullPointerException("The Program-String may not be null");
+        }
         /*
+        // validation happens during parsing now
         if(!validateStatements(this.program))
             throw new IllegalArgumentException("The Program-String contains invalid characters");
         if(!validateBrackets(this.program))
@@ -133,16 +167,20 @@ public final class Interpreter {
         int instructionPointer = 0;
         int pointer = 0;
         int delta;
+        char instruction;
 
         while(instructionPointer < program.length){
 
-            char instruction = program[instructionPointer];
+            instruction = program[instructionPointer];
+            logger.info("Working on Instruction: " + instruction);
 
-            // this handles wrap around
-            if(pointer == -1)
+            if(pointer == -1) {
                 pointer = MAX_MEMORY;
-            else if(pointer == MAX_MEMORY + 1)
+                logger.info("Memory Underflow");
+            }else if(pointer == MAX_MEMORY + 1) {
+                logger.info("Memory Overflow");
                 pointer = 0;
+            }
 
 
             delta = 1;
@@ -170,8 +208,11 @@ public final class Interpreter {
                     memory[pointer] = inputByte[0];
                     break;
                 case '[':
-                    if(memory[pointer] == 0)
+                    logger.fine("Entering Loop");
+                    if(memory[pointer] == 0) {
                         delta = jumpLength(program, instructionPointer) + 1;
+                        logger.info("Exiting Loop");
+                    }
                     break;
                 case ']':
                     // always jump back to the last [
@@ -180,6 +221,8 @@ public final class Interpreter {
                 default:
                     throw new IllegalArgumentException("The Instruction: " + instruction + " is invalid");
             }
+            logger.info(String.format("Pointer: %d\tMemory: %d", pointer, memory[pointer]));
+            logger.fine(String.format("Jumping towards %d", instructionPointer + delta));
             instructionPointer += delta;
         }
 
--- src/test/java/InterpreterTest.java
+++ src/test/java/InterpreterTest.java
@@ -11,45 +11,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InterpreterTest {
 
-    Interpreter testbox =  new Interpreter();
-
-
-
+    Interpreter testbox = new Interpreter();
 
 
     String[] programSet_JumpLength = {"012[4567]9",
-                                             "012[4[6]8]",
-                                             "[1234]6[8]",
-                                             "[]"        ,
-                                             "[[[]]]1234",};
-
-
+            "012[4[6]8]",
+            "[1234]6[8]",
+            "[]",
+            "[[[]]]1234",};
     byte[] desiredResults_JumpLength = {5, 6, 5, 1, 3};
-    int[]  starts = {3,3,0,0,1};
-
-    public void executeTestCases(List<String> testCases, List<String> desiredResults){
-
-
-    }
-
+    int[] starts = {3, 3, 0, 0, 1};
 
 
     @DisplayName("jumpLength")
     @Test
-    public void testJumpLengthCase(){
+    void testJumpLength() {
         int start, len0, len1;
         char[] program;
-        for(int i=0; i<programSet_JumpLength.length;i++){
+        String errorOut;
+        for (int i = 0; i < programSet_JumpLength.length; i++) {
             program = programSet_JumpLength[i].toCharArray();
             start = starts[i];
             len0 = testbox.jumpLength(program, start);
-            System.out.println(Integer.toString(len0) + " " + Integer.toString(desiredResults_JumpLength[i]));
-            assertEquals(len0, desiredResults_JumpLength[i], String.format("Failed to compute the length of a jump towards ] (%d)", i));
+            errorOut = "Failed to compute the length of a jump towards ] \n" + new String(program);
+            assertEquals(len0, desiredResults_JumpLength[i],errorOut);
             len1 = testbox.jumpLength(program, start + len0);
-            assertEquals(len0, -len1,"Failed to compute the length of a jump towards [");
+            errorOut = "Failed to compute the length of a jump towards [ \n" + new String(program);
+            assertEquals(len0, -len1, errorOut);
         }
     }
 
 
-
+    @Test
+    void testInterpret(){
+        String programString = ">>>>>>";
+        testbox.interpret(programString.toCharArray());
+    }
 }
+
